00aa63fa1e606b2ef2106c3f984e71624a5d35f6
Peter Evance <peterevance1@gmail.com>
2023-12-17 09:44:09 +0300

    Added dependencies and code refactor...

--- users/urls.py
+++ users/urls.py
@@ -13,8 +13,8 @@ router.register(r"users", CustomUserViewSet, basename="users")
 # Define URL patterns for the 'users' app
 urlpatterns = [
     # URL for user authentication (login)
-    path("/auth/login/", TokenCreateView.as_view(), name="login"),
+    path("auth/login/", TokenCreateView.as_view(), name="login"),
     # URL for user logout
-    path("/auth/logout/", TokenDestroyView.as_view(), name="logout"),
+    path("auth/logout/", TokenDestroyView.as_view(), name="logout"),
     path("", include(router.urls)),
 ]
