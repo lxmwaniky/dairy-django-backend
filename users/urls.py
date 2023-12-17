473e472f5687f094951b5a345038e708da593ed0
Peter Evance <peterevance1@gmail.com>
2023-12-17 09:27:10 +0300

    Testcases, and code refactors

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,20 @@
-from django.urls import path
+from django.urls import path, include
 from djoser.views import TokenCreateView, TokenDestroyView
+from rest_framework import routers
+
+from users.views import CustomUserViewSet
 
 # Set the app name for namespacing
 app_name = "users"
 
+router = routers.DefaultRouter()
+router.register(r"users", CustomUserViewSet, basename="users")
+
 # Define URL patterns for the 'users' app
 urlpatterns = [
     # URL for user authentication (login)
-    path("api/login/", TokenCreateView.as_view(), name="login"),
+    path("/auth/login/", TokenCreateView.as_view(), name="login"),
     # URL for user logout
-    path("api/logout/", TokenDestroyView.as_view(), name="logout"),
+    path("/auth/logout/", TokenDestroyView.as_view(), name="logout"),
+    path("", include(router.urls)),
 ]
