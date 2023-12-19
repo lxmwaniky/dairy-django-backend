5cb977c3a50d001bc6c0efaeb86f0069742996bb
Peter Evance <peterevance1@gmail.com>
2023-12-19 19:15:46 +0300

    Choices addition and refactor

--- users/choices.py
+++ users/choices.py
@@ -1,16 +1,7 @@
 from django.db import models
 
 
-# Define choices for the 'Sex' field in your models
+# Choices for the 'Sex' field in your models
 class SexChoices(models.TextChoices):
     MALE = "Male"
     FEMALE = "Female"
-
-# choices for the 'Breed' field in your models
-class BreedChoices(models.TextChoices):
-    FRIESIAN = 'Friesian', 'Friesian'
-    SAHIWAL = 'Sahiwal', 'Sahiwal'
-    JERSEY = 'Jersey', 'Jersey'
-    GUERNSEY = 'Guernsey', 'Guernsey'
-    CROSSBREED = 'Crossbreed', 'Crossbreed'
-    AYRSHIRE = 'Ayrshire', 'Ayrshire'    
