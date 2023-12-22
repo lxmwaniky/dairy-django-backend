477f3b2cfc9b5d7d887518df0066d807fea64a98
Peter Evance <peterevance1@gmail.com>
2023-12-22 19:09:52 +0300

    Revised `Cow` Model

--- core/models.py
+++ core/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
+from core.choices import (
+    CowAvailabilityChoices,
+    CowBreedChoices,
+    CowCategoryChoices,
+    CowPregnancyChoices,
+    CowProductionStatusChoices,
+)
+from core.managers import CowManager
+from core.validators import CowValidator, CowBreedValidator
 from users.choices import SexChoices
-from core.choices import CowBreedChoices
-from core.validators import CowBreedValidato, CowValidatorr
-from datetime import date
-import uuid
 
 
 class CowBreed(models.Model):
@@ -28,104 +33,140 @@ class CowBreed(models.Model):
         CowBreedValidator.validate_breed_name(self.name)
         super().save(*args, **kwargs)
 
+
 class Cow(models.Model):
     """
-    Cow Model
-
-    Represents an individual cow in a Django application.
+    Represents an individual cow in the dairy farm.
 
     Attributes:
-    - breed (CharField): The breed of the cow, selected from predefined choices in BreedChoices.
-    - tag_number (CharField): Uniquely identifies each cow with a system-generated tag number.
-    - parity (PositiveIntegerField): Indicates the parity or number of calvings the cow has undergone.
-    - age (PositiveIntegerField): Represents the age of the cow in years.
-    - date_of_birth (DateField): Stores the date of birth of the cow.
-    - name (CharField): Holds the name assigned to the cow.
-    - gender (CharField): Specifies the gender of the cow (e.g., male, female).
-    - availability_status (BooleanField): Indicates whether the cow is currently available.
-    - current_pregnancy_status (CharField): Tracks the current pregnancy status of the cow.
-    - category (CharField): Categorizes the cow (e.g., calf, heifer, milking cow).
-    - current_production_status (CharField): Captures the current production status of the cow (e.g., milking, weaning).
-    - date_introduced_in_farm (DateField): Records the date the cow was introduced to the farm.
-    - is_bought (BooleanField): Indicates whether the cow was purchased.
-    - date_of_death (DateField, Nullable): Records the date of death if applicable.
-    - sire (CharField): Specifies the sire (father) of the cow.
-    - dam (CharField): Specifies the dam (mother) of the cow.
+    - `name` (str): The name of the cow.
+    - `breed` (CowBreed): The breed of the cow.
+    - `date_of_birth` (date): The birthdate of the cow.
+    - `gender` (str): The gender of the cow.
+    - `availability_status` (str): The availability status of the cow.
+    - `sire` (Cow or None): The sire (father) of the cow.
+    - `dam` (Cow or None): The dam (mother) of the cow.
+    - `current_pregnancy_status` (str): The current pregnancy status of the cow.
+    - `category` (str): The category of the cow.
+    - `current_production_status` (str): The current production status of the cow.
+    - `date_introduced_in_farm` (date): The date the cow was introduced to the farm.
+    - `is_bought` (bool): Indicates whether the cow was bought or not.
+    - `date_of_death` (date or None): The date of death of the cow, if applicable.
     """
 
-class Cow(models.Model):
-
-    breed = models.CharField(
-        max_length=20,
-        choices=BreedChoices.choices,
-        blank=False,
-    )
-
-    tag_number = models.CharField(max_length=255, primary_key=True, editable=False)
-
-    parity = models.PositiveIntegerField()
-    age = models.PositiveIntegerField()
-
-    # Field to store the date of birth of the cow.
+    name = models.CharField(max_length=35)
+    breed = models.ForeignKey(CowBreed, on_delete=models.PROTECT, related_name="cows")
     date_of_birth = models.DateField()
-
-    # Field for the name of the cow.
-    name = models.CharField(max_length=255)
-
-    # Field to specify the gender of the cow.
-    gender = models.CharField(
-        max_length=10,
-        choices=SexChoices.choices,
-        blank=False,
+    gender = models.CharField(max_length=6, choices=SexChoices.choices)
+    availability_status = models.CharField(
+        choices=CowAvailabilityChoices.choices,
+        default=CowAvailabilityChoices.ALIVE,
+        max_length=5,
     )
-
-    # Field to indicate the availability status of the cow.
-    availability_status = models.BooleanField(default=True)
-
-    # Field to track the current pregnancy status of the cow.
-    current_pregnancy_status = models.CharField(max_length=50, blank=True)
-
-    # Field to categorize the cow (e.g., calf, heifer, milking cow).
-    category = models.CharField(max_length=50)
-
-    # Field to capture the current production status of the cow.
-    current_production_status = models.CharField(max_length=50)
-
-    # Field to record the date the cow was introduced to the farm.
-    date_introduced_in_farm = models.DateField()
-
-    # Field to indicate whether the cow was bought.
+    current_pregnancy_status = models.CharField(
+        choices=CowPregnancyChoices.choices,
+        default=CowPregnancyChoices.UNAVAILABLE,
+        max_length=12,
+    )
+    category = models.CharField(
+        choices=CowCategoryChoices.choices,
+        default=CowCategoryChoices.CALF,
+        max_length=11,
+    )
+    current_production_status = models.CharField(
+        choices=CowProductionStatusChoices.choices,
+        max_length=22,
+        default=CowProductionStatusChoices.CALF,
+    )
     is_bought = models.BooleanField(default=False)
+    sire = models.ForeignKey(
+        "self", on_delete=models.SET_NULL, null=True, related_name="offspring"
+    )
+    dam = models.ForeignKey(
+        "self", on_delete=models.SET_NULL, null=True, related_name="calves"
+    )
+    date_introduced_in_farm = models.DateField(auto_now=True)
 
-    # Field to capture the date of death if applicable.
-    date_of_death = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField(null=True)
 
-    # Field to specify the sire of the cow.
-    sire = models.CharField(max_length=255, blank=True)
+    objects = CowManager()
 
-    # Field to specify the dam of the cow.
-    dam = models.CharField(max_length=255, blank=True)
+    @property
+    def tag_number(self):
+        """
+        Returns the tag number of the cow.
+        """
+        return Cow.objects.get_tag_number(self)
 
-    # Field for the age in farm
-    age_in_farm = property(calculate_age_in_farm)
+    @property
+    def age(self):
+        """
+        Calculates and returns the age of the cow in days.
+        """
+        return Cow.objects.calculate_age(self)
 
-    def calculate_age_in_farm(self):
-        today = date.today()
-        introduced_date = self.date_introduced_in_farm
-        age_in_farm = today.year - introduced_date.year - ((today.month, today.day) < (introduced_date.month, introduced_date.day))
-        return age_in_farm
+    @property
+    def age_in_farm(self):
+        """
+        Calculates and returns the age of the cow in days since introduction to the farm.
+        """
+        return Cow.objects.calculate_age_in_farm(self)
+
+    @property
+    def calf_records(self):
+        return Cow.objects.get_calf_records(self)
 
     def clean(self):
-        # Use CowValidator for validation checks
-        CowValidator.validate_category_production_status(self)
-        CowValidator.validate_positive_age(self.age)
-        CowValidator.validate_introduction_date(self.date_introduced_in_farm)
+        """
+        Performs validation checks before saving the cow.
+
+        Raises:
+        - `ValidationError`: If cow validation fails.
+
+        """
+
+        if self.pk:
+            CowValidator.validate_production_status(
+                self.current_production_status,
+                self.gender,
+                self.category,
+                self.age,
+                self.calf_records,
+                self.is_bought,
+                self,
+            )
+        else:
+            CowValidator.validate_pregnancy_status(
+                self,
+                self.age,
+                self.current_pregnancy_status,
+                self.availability_status,
+                self.gender, )
+            CowValidator.validate_uniqueness(self.name)
+            CowValidator.validate_cow_age(self.age, self.date_of_birth)
+            CowValidator.validate_gender_update(self.pk, self.gender)
+            CowValidator.validate_sire_dam_relationship(self.sire, self.dam)
+            CowValidator.validate_pregnancy_status(
+                self,
+                self.age,
+                self.current_pregnancy_status,
+                self.availability_status,
+                self.gender,
+            )
+
+            CowValidator.validate_date_of_death(
+                self.availability_status, self.date_of_death
+            )
+
+    def __str__(self):
+        """
+        Returns a string representation of the cow.
+        """
+        return self.tag_number
 
     def save(self, *args, **kwargs):
-        # Generate a unique tag number using a combination of date and UUID
-        if not self.tag_number:
-            today_str = date.today().strftime("%Y%m%d")
-            uuid_str = str(uuid.uuid4().hex)[:6]  # Use the first 6 characters of UUID
-            self.tag_number = f"{today_str}-{uuid_str}"
+        """
+        Overrides the save method to ensure validation before saving.
+        """
         self.clean()
         super().save(*args, **kwargs)
