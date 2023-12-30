07e3c5f00edb7720f42adb7fd557b3ef62b61439
Peter Evance <peterevance1@gmail.com>
2023-12-30 18:21:21 +0300

    Added Weight Record model

--- health/models.py
+++ health/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+from core.models import Cow
+from health.validators import WeightRecordValidator
+
+
+class WeightRecord(models.Model):
+    """
+    Represents a weight record for a cow.
+
+    Attributes:
+    - `cow` (Cow): The cow associated with the weight record.
+    - `weight_in_kgs` (Decimal): The weight of the cow in kilograms.
+    - `date_taken` (Date): The date when the weight record was taken.
+
+    Methods:
+    - `__str__`: Returns a string representation of the weight record.
+    - `clean`: Performs validation checks before saving the weight record.
+    - `save`: Overrides the save method to ensure validation before saving.
+
+    Raises:
+    - `ValidationError`: If weight record validation fails.
+    """
+
+    cow = models.ForeignKey(Cow, on_delete=models.CASCADE)
+    weight_in_kgs = models.DecimalField(max_digits=6, decimal_places=2)
+    date_taken = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        """
+        Returns a string representation of the weight record.
+        """
+        return f"{self.cow} - Weight: {self.weight_in_kgs} kgs - Date: {self.date_taken}"
+
+    def clean(self):
+        """
+        Performs validation checks before saving the weight record.
+
+        Raises:
+        - `ValidationError`: If weight record validation fails.
+        """
+        WeightRecordValidator.validate_weight(self.weight_in_kgs)
+        WeightRecordValidator.validate_cow_availability_status(self.cow)
+        WeightRecordValidator.validate_frequency_of_weight_records(self.date_taken, self.cow)
+
+    def save(self, *args, **kwargs):
+        """
+        Overrides the save method to ensure validation before saving.
+        """
+        self.clean()
+        super().save(*args, **kwargs)
+
