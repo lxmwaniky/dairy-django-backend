834b489766b0c21d127f72e369a623af6ddd0b88
Peter Evance <peterevance1@gmail.com>
2024-01-01 19:00:06 +0300

    Added Culling Record serializer.

--- health/serializers.py
+++ health/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from core.models import Cow
-from health.models import WeightRecord
+from health.models import WeightRecord, CullingRecord
 
 
 class WeightRecordSerializer(serializers.ModelSerializer):
@@ -40,3 +40,44 @@ class WeightRecordSerializer(serializers.ModelSerializer):
     class Meta:
         model = WeightRecord
         fields = ("cow", "weight_in_kgs", "date_taken")
+
+
+class CullingRecordSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the CullingRecord model.
+
+    Fields:
+    - `cow`: A primary key related field representing the cow associated with the culling record.
+    - `reason`: A field representing the reason for culling, chosen from predefined choices.
+    - `notes`: A text field representing additional notes or comments about the culling.
+    - `date_carried`: A date field representing the date when the culling record was created.
+
+    Meta:
+    - `model`: The CullingRecord model for which the serializer is defined.
+    - `fields`: The fields to include in the serialized representation.
+
+    Usage:
+        Use this serializer to convert CullingRecord model instances to JSON representations
+        and vice versa.
+
+    Example:
+        ```python
+        class CullingRecord(models.Model):
+            cow = models.OneToOneField(Cow, on_delete=models.CASCADE, related_name="culling_record")
+            reason = models.CharField(max_length=35, choices=CullingReasonChoices.choices)
+            notes = models.TextField(null=True, max_length=100)
+            date_carried = models.DateField(auto_now_add=True)
+
+        class CullingRecordSerializer(serializers.ModelSerializer):
+            class Meta:
+                model = CullingRecord
+                fields = ("cow", "reason", "notes", "date_carried")
+        ```
+
+    """
+
+    cow = serializers.PrimaryKeyRelatedField(queryset=Cow.objects.all())
+
+    class Meta:
+        model = CullingRecord
+        fields = ("cow", "reason", "notes", "date_carried")
