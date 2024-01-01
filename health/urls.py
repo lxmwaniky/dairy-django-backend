cf2f165cc47810e134686dd0c4922c46dc9e5d56
Peter Evance <peterevance1@gmail.com>
2024-01-01 19:01:17 +0300

    Registered URL...

--- health/urls.py
+++ health/urls.py
@@ -2,15 +2,14 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from health.views import WeightRecordViewSet
+from health.views import WeightRecordViewSet, CullingRecordViewSet
 
 app_name = 'health'
 
 router = routers.DefaultRouter()
 router.register(r'weight-records', WeightRecordViewSet, basename='weight-records')
+router.register(r'culling-records', CullingRecordViewSet, basename='culling-records')
 
 urlpatterns = [
     path('', include(router.urls))
 ]
-
-
